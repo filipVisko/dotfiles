d2d1f6492430ba0de997727dc343ca89535a3c66
filipVisko <10026776+filipVisko@users.noreply.github.com>
2025-11-23 19:45:06 +0000

    init

---
+++ dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+# File system
+alias decompress "tar -xzf"
+alias ff "fzf --preview 'bat --style=numbers --theme=dark --color=always {}'"
+alias ls "eza -lh --group-directories-first"
+alias lsa "ls -a"
+alias lt "eza --tree --level=1 --long --icons --git"
+alias lta "lt -a"
